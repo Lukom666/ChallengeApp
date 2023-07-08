46756b93a099b82e359d569725999768259fd8e8
l.lukomski90@gmail.com <l.lukomski90@gmail.com>
2023-07-08 18:45:27 +0200

    Zadanie dzień 3

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello!");
+﻿var name = "Łukasz";
+var sex = "man";
+var age = 33;
+
+
+if (name == "Łukasz" && age < 30 && age > 18)
+{
+    if (sex == "man")
+    {
+        Console.WriteLine("Łukasz, mężczyzna poniżej 30 lat");
+    }
+    else
+    {
+        Console.WriteLine("kobieta poniżej 30 lat");
+    }
+}
+else if (age == 33)
+{
+    if (sex == "man")
+    {
+        Console.WriteLine("Łukasz, mężczyzna lat 33 lat");
+    }
+    else
+    {
+        Console.WriteLine("kobieta lat 33 lat");
+    }
+}
+else if (age < 18)
+{
+    if (sex == "man")
+    {
+        Console.WriteLine("Niepełnoletni mężczyzna");
+    }
+    else
+    {
+        Console.WriteLine("Niepełnoletnia kobieta");
+    }
+}
+else if (age > 30)
+{
+    if (sex == "man")
+    {
+        Console.WriteLine("Łukasz, doświadczony mężczyzna");
+    }
+    else
+    { 
+        Console.WriteLine("Doświadczona kobieta");
+    }
+}
