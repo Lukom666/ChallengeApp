7023b77a193c8850e51140653a5cdcd750fd702a
l.lukomski90@gmail.com <l.lukomski90@gmail.com>
2023-07-20 00:30:00 +0200

    Zadanie 8

--- ChallengeApp/ChallengeApp.Test/EmployeesTests.cs
+++ ChallengeApp/ChallengeApp.Test/EmployeesTests.cs
@@ -1,55 +1,26 @@
+using System.Reflection.Metadata;
+
 namespace ChallengeApp.Test
 {
+
     public class Tests
     {
         [Test]
-        public void employee0Test()
+        public void Test1()
         {
-            var employee0 = new Employee("Josh", "Good", "48");
-            employee0.AddRating(8);
-            employee0.AddRating(5);
-            employee0.AddRating(7);
-            employee0.AddRating(6);
-            employee0.AddRating(7);
-            employee0.AddRating(1);
 
-            var result = employee0.Result;
+            // arrange
+            int age1 = 10;
+            int age2 = 3;
 
-            Assert.AreEqual(34, result);
-        }
-        [Test]
-        public void employee1Test()
-        {
-            var employee1 = new Employee("Josh", "Good", "48");
-            employee1.AddRating(8);
-            employee1.AddRating(-5);
-            employee1.AddRating(7);
-            employee1.AddRating(5);
-            employee1.AddRating(4);
-            employee1.AddRating(8);
 
-            var result = employee1.Result;
+            // act
+            int result = age1 + age2;
 
-            Assert.AreEqual(27, result);
+            // assert
+            Assert.AreEqual(13, result);
 
         }
-        [Test]
-        public void employee2Test()
-        {
-            var employee2 = new Employee("Josh", "Good", "48");
-            employee2.AddRating(-3);
-            employee2.AddRating(3);
-            employee2.AddRating(0);
-            employee2.AddRating(-1);
-            employee2.AddRating(-5);
-            employee2.AddRating(-6);
-
-            var result = employee2.Result;
-
-            Assert.AreEqual(-12, result);
-
-        }
-
 
     }
 }
---
+++ ChallengeApp/ChallengeApp.Test/TypeTests.cs
@@ -0,0 +1,43 @@
+﻿
+namespace ChallengeApp.Test
+{
+    public class TypeTests
+    {
+        [Test]
+        public void CheckingIfNumbersAreEqual()
+        {
+            int number1 = 1;
+            int number2 = 1;
+
+            Assert.AreEqual(number1, number2);
+        }
+        [Test]
+        public void CheckingIfFloatingNumbersAreNotEqual()
+        {
+            float number3 = 7.35F;
+            float number4 = 7.43F;
+
+            Assert.AreNotEqual(number3, number4);
+        }
+        [Test]
+        public void CheckingifStringNameAreEqual()
+        {
+            string name0 = ("Lukas");
+            string name1 = ("Lukas");
+
+            Assert.AreEqual(name0, name1);
+        }
+        [Test]
+        public void CheckingIfEmployeesAreDifferentUser()
+        {
+            var employee0 = GetEmployee("Lukas");
+            var employee1 = GetEmployee("Lukas");
+
+            Assert.AreNotEqual(employee0, employee1);
+        }
+        private Employee GetEmployee(string name)
+        {
+            return new Employee(name, name, name);
+        }
+    }
+}
